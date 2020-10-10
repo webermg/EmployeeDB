c805ff757e4008a545fe7fc3460667636471885d
webermg <matthew.gerald.weber@gmail.com>
2020-10-10 13:08:21 -0700

    added page routes

---
+++ server/util/htmlRoutes.js
@@ -0,0 +1,31 @@
+// ===============================================================================
+// DEPENDENCIES
+// ===============================================================================
+const express = require("express");
+const router = express.Router();
+var path = require("path");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+
+  // HTML GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases the user is shown an HTML page of content
+  // ---------------------------------------------------------------------------
+
+  router.get("/analytics", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/analytics.html"));
+  });
+
+  router.get("/view", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/view.html"));
+  });
+
+  router.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../public/index.html"));
+  });
+
+  module.exports = router;
