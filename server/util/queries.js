0e7ec2b4758aefc1e965ed9a20237534c497a66e
webermg <matthew.gerald.weber@gmail.com>
2020-10-10 13:55:18 -0700

    added query to get with one condition

--- server/util/queries.js
+++ server/util/queries.js
@@ -1,20 +1,22 @@
 const connection = require("./db");
 
 const queries = {
-  getData: function() { 
-    
-
+  getAllFromTable: function(table) { 
     return new Promise(function(resolve,reject) {
-      connection.query("SELECT * FROM employees", function(err, res) {
-        if (err) throw err;
-        console.table(res);
+      connection.query("SELECT * FROM ??", table, function(err, res) {
+        if (err) reject(err);
         resolve(res);
       });
     });
+  },
 
-
-
-    
+  getAllWhere: function(table, column, value) {
+    return new Promise(function(resolve,reject) {
+      connection.query("SELECT * FROM ?? WHERE ?? = ?", [table,column,value], function(err, res) {
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
   }
 }
 
