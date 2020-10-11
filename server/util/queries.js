658c7930a07984fc3b335e66d25fe5d974cf5a93
webermg <matthew.gerald.weber@gmail.com>
2020-10-10 22:44:00 -0700

    added post queries

--- server/util/queries.js
+++ server/util/queries.js
@@ -7,9 +7,9 @@ const viewEmployeePath = path.join(__dirname,"../../db/view/allempq.sql");
 const viewRolesPath = path.join(__dirname,"../../db/view/allroleq.sql");
 const viewDeptPath = path.join(__dirname,"../../db/view/alldeptq.sql");
 
-const addEmployeePath = path.join(__dirname,"../../db/view/alldeptq.sql");
-const addRolePath = path.join(__dirname,"../../db/view/alldeptq.sql");
-const addDepartmentPath = path.join(__dirname,"../../db/view/alldeptq.sql");
+const addEmployeePath = path.join(__dirname,"../../db/insert/AddEmp.sql");
+const addRolePath = path.join(__dirname,"../../db/insert/addRole.sql");
+const addDepartmentPath = path.join(__dirname,"../../db/insert/addDept.sql");
 
 
 const queries = {
@@ -35,14 +35,34 @@ const queries = {
     });
   },
 
-  getRoles: function() {
+  //test
+  test: function(title,salary,department) {
     return new Promise(function(resolve,reject) {
-      connection.query("SELECT roles.salary,roles.title,departments.name FROM roles,departments WHERE roles.department_id=departments.id;", function(err, res) {
+      connection.query("INSERT INTO roles (title, salary, department_id) VALUES(?,?,(SELECT id FROM departments WHERE name=?));", [title,salary,department], function(err, res) {
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
+  },
+
+  postNewDept: function(deptName) {
+    return new Promise(function(resolve,reject) {
+      connection.query(fs.readFileSync(addDepartmentPath,"utf-8"), {name: deptName}, function(err, res) {
+        if (err) reject(err);
+        resolve(res);
+      });
+    });
+  },
+
+  postNewRole: function(title,salary,deptName) {
+    return new Promise(function(resolve,reject) {
+      connection.query(fs.readFileSync(addRolePath,"utf-8"), [title,salary,deptName], function(err, res) {
         if (err) reject(err);
         resolve(res);
       });
     });
   }
+
 }
 
 module.exports = queries;
