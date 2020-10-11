883ee36c218a7906b877be38f7cfcb0856e33582
webermg <matthew.gerald.weber@gmail.com>
2020-10-11 11:18:48 -0700

    added query

---
+++ db/insert/addEmp.sql
@@ -0,0 +1,2 @@
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES (?, ?, (SELECT id FROM roles WHERE title=?), (SELECT e.id from (SELECT id FROM employees WHERE first_name=? AND last_name=?) as e));
