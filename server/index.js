1884628d396044bb427799a1e003267b2fd75b4b
webermg <matthew.gerald.weber@gmail.com>
2020-10-11 21:28:10 -0700

    fixed static path

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 3000;
 // Config
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname + ".." + "public")));
+app.use(express.static(path.join(__dirname,"/../public")));
 
 //routes
 const htmlRoutes = require("./util/htmlRoutes");
