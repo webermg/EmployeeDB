537aaf4f0fe687b989e05bc5e2a7e288d057f657
webermg <matthew.gerald.weber@gmail.com>
2020-10-12 18:53:30 -0700

    added pie chart for costs by department

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,6 +1,10 @@
-var cht1 = $('#chart1');
-var cht2 = $('#chart2');
-var cht3 = $('#chart3');
+const cht1 = $('#chart1');
+const cht2 = $('#chart2');
+const cht3 = $('#chart3');
+
+const colors = ['red','blue','green','yellow','orange','purple','brown','silver','tan','pink'];
+
+let chart1;
 
 const getSalaryData = () => {
     return $.ajax({
@@ -9,35 +13,7 @@ const getSalaryData = () => {
     });
   };
 
-var myChart = new Chart(cht1, {
-  type: 'bar',
-  data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-          label: '# of Votes',
-          data: [12, 19, 3, 5, 2, 3],
-          backgroundColor: function(context) {
-            let r = Math.floor(Math.random()*255);
-            let g = Math.floor(Math.random()*255);
-            let b = Math.floor(Math.random()*255);
-            return `rgba(${r}, ${g}, ${b}, 1)`;
-            },
-          borderColor: function(context) {
-            return 'black;'
-          },
-          borderWidth: 1
-      }]
-  },
-  options: {
-      scales: {
-          yAxes: [{
-              ticks: {
-                  beginAtZero: true
-              }
-          }]
-      }
-  }
-});
+
 
 var myChart = new Chart(cht2, {
   type: 'pie',
@@ -69,8 +45,29 @@ var myChart = new Chart(cht2, {
   }
 });
 
+const createChart = (chart, type,data,label) => {
+    const labels = [], values = [];
+    data.forEach(e => {
+        labels.push(e.name);
+        values.push(e.value);
+    })
+    return new Chart(chart,{
+        type: type,
+        data: {
+            labels: labels,
+            datasets: [{
+                label: label,
+                data: values,
+                backgroundColor: function(context) {
+                    return colors[context.dataIndex % colors.length];
+                },
+            }]
+        }
+    })
+}
+
 $(document).ready(() => {
     getSalaryData().then(result => {
-        console.log(result);
+        chart1 = createChart(cht1,"pie",result,"Costs by Department");
     })
   });
