48fa147786b09c5be5938c33bcf345ca7db1cb39
webermg <matthew.gerald.weber@gmail.com>
2020-10-12 20:53:52 -0700

    removed unnecessary import

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@
 // =============================================================
 const express = require("express");
 const path = require("path");
-const db = require("./util/db");
 
 // ==============================================================================
 // EXPRESS CONFIGURATION
